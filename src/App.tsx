d5ea9b2d38fbd04227850abea706e988f2d14258
Becca Shapiro <beccashapiro4@gmail.com>
2024-11-03 17:51:57 -0500

    improve ghost-tail, update to-do list

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import Worm from './WormSegment.tsx'
 TO-DO:
 - fix worm alignment (maybe done?)
 - make clock's position "relative" to size & center of greater webpage
-- add blinking worm eyes
 - add cute little "squiggles" which appear & disappear
+- add some sorta "foggy overlay" through which the squiggles are swimming
+- investigate/fix worm-blink animation glitch
 */
 
 function App() {
