7b78e1f5a4c6f06466d1eb34540b3174dd9c797a
Becca Shapiro <beccashapiro4@gmail.com>
2024-11-03 00:22:22 -0400

    colorful worms :D

--- src/WormSegment.tsx
+++ src/WormSegment.tsx
@@ -14,12 +14,23 @@ function positionAroundCircle(center, radius, i) {
     return ({ x, y });
 };
 
-function WormSegment({ position, angle = 0, id = 'body' }) {
+function hueGivenRadius(radius = 100) {
+    if (radius == 200) {
+        return 120
+    } else if (radius == 250) {
+        return 240
+    } else {
+        return 0
+    };
+}
+
+function WormSegment({ position, angle = 0, hue = 120, id = 'body' }) {
 
     const wormStyle = {
         left: position.x,
         top: position.y,
-        rotate: angle.toString() + 'rad'
+        rotate: angle.toString() + 'rad',
+        filter: 'hue-rotate(' + hue + 'deg)'
     };
 
     return (
@@ -40,12 +51,13 @@ function Worm({ index = 0, center = { x: 200, y: 200 }, radius = 100 }) {
         var i = 1;
         var segments = [];
         var segmentIndex = index
-        segments.push(<WormSegment position={headPosition} angle={headAngle} id='head' />)
+        let hue = hueGivenRadius(radius);
+        segments.push(<WormSegment position={headPosition} angle={headAngle} hue={hue} id='head' />)
         while (i < length) {
             segmentIndex = (segmentIndex - 0.01) % 1;
             let pos = positionAroundCircle(center, radius, segmentIndex);
             let a = (angleGivenIndex(segmentIndex) - Math.PI / 2) % (Math.PI * 2);
-            segments.push(<WormSegment position={pos} angle={a} id='body' />);
+            segments.push(<WormSegment position={pos} angle={a} hue={hue} id='body' />);
             i++
         };
         return segments
