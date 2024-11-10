0cf158c6bf33b97a739d4c47cc5a946433d89358
Becca Shapiro <beccashapiro4@gmail.com>
2024-11-10 15:42:09 -0500

    test test

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+function testOnly() {
+  return (
+    <div>
+      <p> this is a test of the connection. </p>
+    </div>
+  );
+};
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  testOnly()
+  //<App />,
 )
